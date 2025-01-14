ca3f94d8ca89428f2b5a7ff552ff1e2473d20a40
Matviy4ik <matvejbajdik1@gmail.com>
2025-01-14 21:14:57 +0300

    models2

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from sqlalchemy import func, String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
@@ -15,7 +15,6 @@ class User(Base):
     password_hash: Mapped[str] = mapped_column()
 
     transactions = relationship('Transaction', back_populates='user', cascade='all, delete-orphan')
-    categories = relationship('Category', back_populates='user', cascade='all, delete-orphan')
     goals = relationship('Goal', back_populates='user', cascade='all, delete-orphan')
 
 
@@ -23,33 +22,34 @@ class Transaction(Base):
     __tablename__ = 'transactions'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
     amount: Mapped[int] = mapped_column(nullable=False)
-    category: Mapped[str] = mapped_column(String(50), nullable=False)
     date: Mapped[datetime] = mapped_column(server_default=func.now())
     note: Mapped = mapped_column(String(250), nullable=True)
 
     user = relationship('User', back_populates='transactions')
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
+    categories = relationship('Category', back_populates='transactions')
+    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"))
 
 
 class Category(Base):
     __tablename__ = 'categories'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
     name: Mapped[str] = mapped_column(String(50), nullable=False)
 
-    user = relationship('User', back_populates='categories')
+    transactions = relationship('Transaction', back_populates='categories')
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
 
 
 class Goal(Base):
     __tablename__ = 'goals'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     target_amount: Mapped[int] = mapped_column(nullable=False)
     current_amount: Mapped[int] = mapped_column(default=0.0)
-    deadline: Mapped[datetime] = mapped_column(nullable=False)
+    deadline: Mapped[datetime] = mapped_column(nullable=True, default=datetime.now() + timedelta(days=30))
 
     user = relationship('User', back_populates='goals')
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
